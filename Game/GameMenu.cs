513bd0b61af916e4951591ef0eb7f2246cf3b074
dadaPipes <markoliverstockflethstaf@gmail.com>
2023-07-05 00:25:53 +0200

    minor updates

--- Game/GameMenu.cs
+++ Game/GameMenu.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Spectre.Console;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,5 +22,4 @@ namespace Game
             return selectedGame;
         }
     }
-}
 }
